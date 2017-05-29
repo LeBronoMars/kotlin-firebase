4d056ebe5ad6a53fc99069e625b70131d28e52d2
Russel <rsbulanon@Russels-MacBook-Pro.local>
2017-05-29 18:21:32 +0800

    Added firebase messaging

---
+++ app/src/main/java/proto/com/kotlinapp/services/firebase/MyFirebaseInstanceIDService.kt
@@ -0,0 +1,10 @@
+package proto.com.kotlinapp.services.firebase
+
+import com.google.firebase.iid.FirebaseInstanceIdService
+
+/**
+ * Created by rsbulanon on 5/29/17.
+ */
+class MyFirebaseInstanceIDService : FirebaseInstanceIdService() {
+
+}
---
+++ app/src/main/java/proto/com/kotlinapp/services/firebase/MyFirebaseMessagingService.kt
@@ -0,0 +1,46 @@
+package proto.com.kotlinapp.services.firebase
+
+import android.app.NotificationManager
+import android.app.PendingIntent
+import android.content.Context
+import android.content.Intent
+import android.media.RingtoneManager
+import android.support.v4.app.NotificationCompat
+import com.google.firebase.messaging.FirebaseMessagingService
+import com.google.firebase.messaging.RemoteMessage
+import proto.com.kotlinapp.R
+import proto.com.kotlinapp.activities.HomeActivity
+
+
+/**
+ * Created by rsbulanon on 5/29/17.
+ */
+class MyFirebaseMessagingService : FirebaseMessagingService() {
+
+    override fun onMessageReceived(p0: RemoteMessage?) {
+        super.onMessageReceived(p0)
+        p0?.notification?.apply {
+            sendNotification(title.toString(), body.toString())
+        }
+    }
+
+    private fun sendNotification(title: String, messageBody: String) {
+        val intent = Intent(this, HomeActivity::class.java)
+        intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP)
+        val pendingIntent = PendingIntent.getActivity(this, 0 /* Request code */, intent,
+                PendingIntent.FLAG_ONE_SHOT)
+
+        val defaultSoundUri = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION)
+        val notificationBuilder = NotificationCompat.Builder(this)
+                .setSmallIcon(R.mipmap.ic_launcher)
+                .setContentTitle(title)
+                .setContentText(messageBody)
+                .setAutoCancel(true)
+                .setSound(defaultSoundUri)
+                .setContentIntent(pendingIntent)
+
+        val notificationManager = getSystemService(Context.NOTIFICATION_SERVICE) as NotificationManager
+
+        notificationManager.notify(0 /* ID of notification */, notificationBuilder.build())
+    }
+}
